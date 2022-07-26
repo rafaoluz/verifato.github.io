04ee187fcedb4aa49287bb077bf51d384b2cee91
Rafael Luz <rafaoluz@hotmail.com>
2022-07-26 19:34:44 -0300

    link

--- js/script.js
+++ js/script.js
@@ -13,9 +13,7 @@ function initAccordion() {
     const activeClass = 'ativo';
 
     if (accordionList.length) {
-        accordionList[0].classList.add(activeClass);
-        accordionList[0].nextElementSibling.classList.add(activeClass);
-
+        
         function activeAccordion() {
             this.classList.toggle(activeClass);
             this.nextElementSibling.classList.toggle(activeClass);
